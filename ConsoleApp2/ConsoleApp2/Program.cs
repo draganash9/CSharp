22eb549673735ef34e2c59656dedd14dad9e6f41
Dragana <draganashishkovska@gmail.com>
2019-03-12 14:39:08 +0100

    homework commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//DateTime aDate = DateTime.Now.AddDays(3);
+			//DateTime bDate = DateTime.Now.AddMonths(1);
+			//DateTime cDate = DateTime.Now.AddMonths(1).AddDays(3);
+			//DateTime dDate = DateTime.Now.AddYears(-1).AddMonths(-2);
+			//DateTime month = DateTime.Now;
+			//DateTime year = DateTime.Now;
+			//Console.WriteLine(year.Year);
+			//Console.WriteLine(month.Month);
+			//Console.WriteLine(month.ToString("MMMM"));
+			//Console.WriteLine(dDate);
+			//Console.ReadLine();
+			//AgeCalculator();
+
+			//Console.ReadLine();
+			
+			Console.WriteLine("Enter your birthdate in format: month-day-year");
+			DateTime birthday = DateTime.Parse(Console.ReadLine());
+			AgeCalculator(birthday);
+			
+
+		}
+		
+		static void AgeCalculator(DateTime birthdate)
+		{
+			DateTime currentTime = DateTime.Now;
+			int age = currentTime.Year - birthdate.Year;
+			if (birthdate.Month > currentTime.Month) age--;
+			if (birthdate.Month == currentTime.Month && birthdate.Day > currentTime.Day) age--;
+			Console.WriteLine($"Your age is: {age}");
+			Console.ReadLine();
+		}
+
+
+		
+
+
+
+	}
+}
---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Homework3.classes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+	class Classes
+	{
+		static void Main(string[] args)
+		{
+			Student[] Students = new Student[5];
+			Students[0] = new Student("Martin", "SEDC", "G2");
+			Students[1] = new Student("Dario", "SEDC", "G2");
+			Students[2] = new Student("Petar", "SEDC", "G2");
+			Students[3] = new Student("Vanco", "Semos", "A");
+			Students[4] = new Student("Riste", "Codecademy", "A");
+
+			var Dragana = true;
+			Console.WriteLine("Enter a name");
+			string name = Console.ReadLine();
+			foreach (var item in Students)
+			{
+				if (name==item.Name)
+				{
+					Dragana = false;
+					Console.WriteLine($"{item.Name} {item.Academy} {item.Group}");
+					break;
+				}
+				
+
+			}
+			if (Dragana)
+			{
+				Console.WriteLine("There is not a student with that name");
+			}
+
+			Console.ReadLine();
+
+		}
+
+		
+
+	}
+}
