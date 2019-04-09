28c6d394234f64d9842d67905d6eceb7d4721a4f
draganash9 <48064620+draganash9@users.noreply.github.com>
2019-04-09 13:36:52 +0200

    Add files via upload

---
+++ BreakingNews/ClassLibrary/Classes/Medium.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Classes
+{
+	public class Medium
+	{
+		public delegate void NewsDelegate(object obj, NewsEventArgs e);
+		public event NewsDelegate EventBreakingNews;
+
+		public void SendBreakingNews(string title, string content, DateTime date)
+		{
+			NewsEventArgs BreakingNews = new NewsEventArgs(title, content, date);
+			if (EventBreakingNews == null)
+			{
+				Console.WriteLine("0 Subcribers. Sorry! :(");
+				return;
+			}
+			EventBreakingNews(this, BreakingNews);
+		}
+
+
+
+	}
+
+	public class NewsEventArgs : EventArgs
+	{
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public DateTime Date { get; set; }
+		public NewsEventArgs(string title, string content, DateTime date)
+		{
+			this.Title = title;
+			this.Content = content;
+			this.Date = date;
+		}
+
+	}
+
+}
