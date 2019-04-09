8baabf2f6488e04a67fa8bc4a3ea73517425e537
draganash9 <48064620+draganash9@users.noreply.github.com>
2019-04-09 13:32:23 +0200

    Add files via upload

---
+++ Fighter App/MyLibrary/Classes/ProFighter.cs
@@ -0,0 +1,38 @@
+﻿using MyLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Classes
+{
+    public class ProFighter: Fighter,IStreet,IBox
+    {
+        public ProFighter(double health, double powerPunch, double speed,double experience):base(health,powerPunch,speed)
+        {
+            Experience = experience;
+        }
+        public double Experience { get; set; }
+
+        public void DoBoxing(Fighter opponent)
+        {
+            double dmg = Speed + PowerPunch + Experience;
+            opponent.TakeDamage(dmg);
+
+        }
+
+
+        public void DoStreet(Fighter opponent)
+        {
+            double dmg = Speed + PowerPunch + Experience;
+            FightMethod(opponent, dmg);
+        }
+
+        protected override void Finisher(Fighter opponent)
+        {
+            Console.WriteLine("You hit him with karlicno,expected recovery 5 weeks");
+        }
+      
+    }
+}
---
+++ Fighter App/MyLibrary/Interfaces/IBox.cs
@@ -0,0 +1,17 @@
+﻿using MyLibrary.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Interfaces
+{
+    public interface IBox
+    {
+      void DoBoxing(Fighter opponent);
+    }
+        
+        
+        
+}
